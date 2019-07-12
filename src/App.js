88b648232092f26747612e1ce29a190437fdd093
DECAGON <decagon@MacBook-Pro.local>
2019-07-12 13:40:21 +0100

    feat(core): Add Header component to App Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import AppSection from './elements/AppSection'
 import './App.css'
 
-function App () {
-  return (
-    <div className='App'>
-      <AppSection> This is a new Section </AppSection>
-    </div>
-  )
+
+function App() {
+    return ( <div className = 'App' >
+      <Header>This is a Header</Header>
+      <AppSection>This is a Section</AppSection>
+        </div>
+    )
 }
 
 export default App
