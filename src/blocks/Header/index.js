402a2928a10eb726e18e0851a6232835b8d5cae5
DECAGON <decagon@MacBook-Pro.local>
2019-07-12 20:47:51 +0100

    fix:refactor Header block

--- src/blocks/Header/index.js
+++ src/blocks/Header/index.js
@@ -1,29 +1,14 @@
 import styled from 'styled-components';
 import AppSection from '../../elements/AppSection';
-import Logo from './Logo';
 
-import React, {Fragment} from 'react'
 
-export default function Header() {
-    return ( <div>
-      <HeaderSection>
-          <HeaderLogo/>
-      </HeaderSection>
-    </div>
-    )
-}
-
-const HeaderSection = styled(AppSection)`
+const Header = styled(AppSection)`
     width:100%;
     height: 5rem;
     background-color:#FEFFFF;
     display: grid;
-    grid-template-columns: 1fr 2fr 3fr 4fr;
-
+    grid-template-columns: 1fr 2fr 4fr 3fr 2fr;
 `
 
-const HeaderLogo = styled(Logo) `
-   display: grid;
-   grid-column:2/3;
-`
 
+export default Header;
