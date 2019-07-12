60532ed8d4b80a5398d3841502f745c822315f35
DECAGON <decagon@MacBook-Pro.local>
2019-07-12 20:48:18 +0100

    Create header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from './Logo';
+import Header from '../blocks/Header';
+import Menu from './Menu';
+import Status from './Status';
+
+export default () => {
+  return ( <div>
+    <Header>
+      <Logo />
+      <Menu/>
+      <Status/>
+    </Header>
+  </div>
+  )
+}
