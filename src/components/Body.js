be20dbdc6bbe0ae3255f14b764cf99824a126bee
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-13 23:28:30 +0100

    Add Display and Pagination Components to Body Component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Body from '../blocks/Body';
 import Search from './Search';
+import Display from './Display';
+import Pagination from './Pagination';
 
 export default () => {
   return (
     <Body>
       <Search></Search>
+      <Display></Display>
+      <Pagination></Pagination>
     </Body>
   )
 }
