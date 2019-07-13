fec2fd10a9c951040066841d116343d9305a76c3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-13 23:12:56 +0100

    feat(core):  styling for pagination component

---
+++ src/blocks/Pagination/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Page from './Page';
+
+const Pagination = styled.div `
+  grid-row: 6/7;
+  display:flex;
+  grid-column: 2/3;
+`
+
+Pagination.Page = Page;
+export default Pagination;
