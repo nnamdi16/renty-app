4c568ddfd94019a24ad649d36f8158795063a00a
DECAGON <decagon@MacBook-Pro.local>
2019-07-13 03:30:46 +0100

    Add Info component to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import AppSection from './elements/AppSection';
 import './App.css'
 import Header from './components/Header';
+import Info from './components/Info';
 
 
 function App() {
     return ( <div className = 'App' >
       <Header/>
+      <Info/>
       <AppSection>This is a Section</AppSection>
         </div>
     )
