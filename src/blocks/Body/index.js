97fc4937a69056011e95c490f3e9aea581c90c79
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-13 22:46:33 +0100

    fix(core):Modify the height and add definitions for grid-template-columns

--- src/blocks/Body/index.js
+++ src/blocks/Body/index.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 const Body = styled.div  `
   display:grid;
+  grid-template-rows: 1rem 12rem 1rem 46rem 1.5rem 4rem;
   width:100%;
   background-color:#E6E7E8;
-  grid-template-rows: 1rem 12rem;
-  height:18rem;
+  height:100rem;
+  grid-template-columns: 0.5fr 4fr 0.5fr;
 `
 
 export default Body;
