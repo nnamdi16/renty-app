1192f8164e8853e626295d1d558d0c8a88ba63a1
DECAGON <decagon@MacBook-Pro.local>
2019-07-13 07:26:14 +0100

    feat(core):Create the Body block

--- src/blocks/Body/index.js
+++ src/blocks/Body/index.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+const Body = styled.div  `
+  display:grid;
+  width:100%;
+  background-color:#E6E7E8;
+  grid-template-rows: 1rem 12rem;
+  height:18rem;
+`
+
+export default Body;
