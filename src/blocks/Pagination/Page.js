659307911310fa108da26ae84c3a3392ec66f8c8
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-15 16:48:36 +0100

    Create pagination block comprising of the page block

--- src/blocks/Pagination/Page.js
+++ src/blocks/Pagination/Page.js
@@ -4,6 +4,14 @@
 const Page = styled(Button)`
   width: 3rem;
   height: 3rem;
+  cursor: pointer;
+  color: black;
+  float:right;
+  padding: 8px 16px;
+  text-decoration:none;
+  transition: background-color .3s;
+  border: 3px solid #ddd;
+
 `;
 
 export default Page;
