6f5086764dab53dac653cd03af1bdb21da5303eb
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-17 04:21:13 +0100

    feat(core):Create button block for the Menu block

---
+++ src/blocks/Menu/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import Button from '../../elements/Button';
+
+const HeaderButton = styled(Button).attrs(props => ({
+  width: props.width,
+  height: props.height,
+  color: props.color
+}))`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  background-color: ${props => props.color};
+  border: none;
+  place-self:center;
+  color: #F6EBE0;
+  `;
+
+export default HeaderButton ;
