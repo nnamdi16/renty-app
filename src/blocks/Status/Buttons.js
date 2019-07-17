99698cf5ab57ad38e53623ac6153de5d064bfa50
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-17 04:25:19 +0100

    fix:Add width,color,fontColor props to make button block more resuable

--- src/blocks/Status/Buttons.js
+++ src/blocks/Status/Buttons.js
@@ -1,16 +1,20 @@
 import styled, {css} from 'styled-components';
 import Button from '../../elements/Button';
 
-const Buttons = styled(Button) `
+const Buttons = styled(Button).attrs(props =>({
+  width: props.width || '6rem',
+  color: props.color || '#798088',
+  fontColor: props.fontColor || '#F9FCFD'
+})) `
   font-size:0.8rem;
-  background: ${props => props.register ?'#798088':'#C6CACD'};
-  color: #F9FCFD;
+  background: ${props => props.color};
+  color: ${props => props.fontColor};
   height:2rem;
   border-radius: 3px;
   margin: 0 1em;
   padding: 0.25em 1em;
   transition: 0.5s all ease-out;
-  width: ${props => props.register?'5rem':'4rem'};
+  width: ${props => props.width};
   grid-row: 2/3;
 
   /* ${props => props.register && css `
