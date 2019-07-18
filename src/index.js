710af60f69ef72f3deab3f907a5f3b31dc2553da
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 07:20:22 +0100

    Remove redux store for decoupling

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import logger from 'redux-logger';
-import reducers from './reducers';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Root from "./Root";
+import * as serviceWorker from "./serviceWorker";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers,composeEnhancers(applyMiddleware(thunk,promise,logger) ))
-ReactDOM.render(
-  <Provider store = {store} >
-    <App />
-</Provider>
-, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
