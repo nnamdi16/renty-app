12f5e611f397388bf7812ca9034ac0d2ccf68353
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 22:39:18 +0100

    map the values of pages for pagination rendering

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,26 +1,15 @@
 import React from "react";
 import Pagination from "../blocks/Pagination";
-import { Link } from "react-router-dom";
-
+const pages=[1,2,3,4,5,]
 const App = ({ handleClick }) => {
-  console.log(handleClick);
   return (
     <Pagination className="pagination">
-      <Link to="/?page=1">
-        <Pagination.Page className="active">1</Pagination.Page>
-      </Link>
-      <Link to="/?page=2">
-        <Pagination.Page>2</Pagination.Page>
-      </Link>
-      <Link to="/?page=3">
-        <Pagination.Page>3</Pagination.Page>
-      </Link>
-      <Link to="/?page=4">
-        <Pagination.Page>4</Pagination.Page>
-      </Link>
-      <Link to="/?page=5">
-        <Pagination.Page>5</Pagination.Page>
-      </Link>
+      {
+        pages.map((page)=><button as="button" onClick={() => handleClick(page)}>
+        <Pagination.Page className="active">{page}</Pagination.Page>
+      </button>)
+      }
+      
     </Pagination>
   );
 };
