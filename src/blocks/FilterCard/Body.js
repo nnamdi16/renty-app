9d6aada49ccf6b5e4b6dc33a0a0684857c013476
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 07:29:06 +0100

    Create Body,Section and Text Block for FilterCard.js with styling

---
+++ src/blocks/FilterCard/Body.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Body = styled.div`
+  display: grid;
+  grid-template-rows: 1.5rem 5rem 1.5rem 10rem 1.5rem 5rem 1.5rem 10rem ;
+`;
+
+export default Body;
---
+++ src/blocks/FilterCard/Text.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Text = styled.article`
+  font-size: 0.9rem;
+  color: #92949E;
+`;
+
+export default Text;
