f9068f1f27954f3b5ab5e1babf5e1b24729c48ce
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 22:33:40 +0100

    map the manufacturer list in Category component

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,34 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Category from "../blocks/Category";
 
-export default () => {
+const App = ({ manufacturers, handleSelect, manufacturers_list }) => {
   return (
-      <Category>
-        <Category.Item>
-          <Category.Checkbox type="checkbox" />
-          <Category.Text>Ford</Category.Text>
-          <Category.Count>1</Category.Count>
-        </Category.Item>
-        <Category.Item>
-          <Category.Checkbox type="checkbox" />
-          <Category.Text>Reno</Category.Text>
-          <Category.Count>1</Category.Count>
-        </Category.Item>
-        <Category.Item>
-        <Category.Checkbox type="checkbox" />
-          <Category.Text>Toyota</Category.Text>
-          <Category.Count>1</Category.Count>
-        </Category.Item>
-        <Category.Item >
-        <Category.Checkbox type="checkbox" />
-          <Category.Text>Chevrolet</Category.Text>
-          <Category.Count>1</Category.Count>
-        </Category.Item>
-        <Category.Item > 
-        <Category.Checkbox type="checkbox" />
-          <Category.Text>BMW</Category.Text>
-          <Category.Count>1</Category.Count>
-        </Category.Item>
-      </Category>
+    <Category>
+      {manufacturers_list.map(
+        ({ manufacturer, number }, key) =>
+          number > 0 && (
+            <Category.Item key={key}>
+              <Category.Checkbox
+                type="checkbox"
+                value={manufacturer}
+                checked={[...manufacturers].includes(manufacturer)}
+                name="manufacturers"
+                onChange={handleSelect}
+              />
+              <Category.Text>{manufacturer}</Category.Text>
+              <Category.Count>{number}</Category.Count>
+            </Category.Item>
+          )
+      )}
+    </Category>
   );
 };
+App.propTypes = {
+  manufacturers_list: PropTypes.array.isRequired
+};
+App.defaultProps = {
+  manufacturers_list: []
+};
+export default App;
