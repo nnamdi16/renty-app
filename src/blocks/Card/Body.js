e5aab59039de9171b8e157682e386237ee7a9f80
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 07:22:48 +0100

    fix: Modify the styling

--- src/blocks/Card/Body.js
+++ src/blocks/Card/Body.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
-import Header from './Header';
+import styled from "styled-components";
+import Header from "./Header";
 
 const Body = styled(Header)`
--webkit-border-bottom-right-radius: 6px;
--webkit-border-bottom-left-radius: 6px;
--moz-border-radius-bottomright: 6px;
--moz-border-radius-bottomleft: 6px;
-border-bottom-right-radius: 6px;
-border-bottom-left-radius: 6px;
-`
+  -webkit-border-bottom-right-radius: 6px;
+  -webkit-border-bottom-left-radius: 6px;
+  -moz-border-radius-bottomright: 6px;
+  -moz-border-radius-bottomleft: 6px;
+  border-bottom-right-radius: 6px;
+  border-bottom-left-radius: 6px;
+`;
 
 export default Body;
