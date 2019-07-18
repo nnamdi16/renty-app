5f2231ea2a21a08ed088e21323e37f9ee3da2284
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 07:31:56 +0100

    Create Text and modify InfoCard block

---
+++ src/blocks/InfoCard/Text.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Text = styled.article`
+  font-size: 0.9rem;
+`;
+
+export default Text;
--- src/blocks/InfoCard/index.js
+++ src/blocks/InfoCard/index.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import Text from '../InfoCard/Text';
 
 
 const InfoCard = styled.div `
   grid-row: 1/2;
 `
-
+InfoCard.Text = Text;
 export default InfoCard;
