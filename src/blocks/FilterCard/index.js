3cc8bd15669ccaa52a352b568ea7a83ba68cd34b
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-07-18 07:29:43 +0100

    Modify and update FilterCard block

--- src/blocks/FilterCard/index.js
+++ src/blocks/FilterCard/index.js
@@ -1,7 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import Text from "./Text";
+import Section from "./Section";
+import Body from "./Body";
 
-const FilterCard = styled.div `
-  grid-row:3/4;
-`
+const FilterCard = styled.div`
+  grid-row: 3/4;
+`;
 
+FilterCard.Text = Text;
+FilterCard.Section = Section;
+FilterCard.Body = Body;
 export default FilterCard;
